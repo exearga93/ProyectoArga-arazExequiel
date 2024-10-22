10c677b0c5b15445aa3e95de368a9fbbdf50a76c
Exequiel Argañaraz <exequiel.it93@gmail.com>
2024-10-22 01:48:34 -0300

    se crea la rama proyecto para trabajar en ella

---
+++ js/proyecto.js
@@ -0,0 +1,6 @@
+//Simulación de una encuesta de satisfacción
+/*Descripción:
+Se realiza una encuesta en un restaurante sobre la satisfacción de los clientes, cuyas respuesta se muestran como numeros del 1 al 10, donde:
+1 significa muy insatisfecho 
+10 significa muy satisfecho
+Las respuestas estan cargadas en el siguiente array: [8,5,10,7,6,8,9,10,7,4,6,3,7,8,6,5,4,2,9,10] */
